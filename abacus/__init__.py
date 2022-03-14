ff8099360ac01c89ee8094ea54a9c3aa21cf69f9
Sandorex <rzhw3h@gmail.com>
2022-03-14 22:02:45 +0100

    Increment version

--- abacus/__init__.py
+++ abacus/__init__.py
@@ -1,6 +1,6 @@
 """Abacus is an interactive calculator using IPython"""
-__version__ = '0.1.0'
-__version_info__ = (0, 1, 0)
+__version__ = '0.1.1'
+__version_info__ = (0, 1, 1)
 
 from .extensions.abacus.abacus import load_ipython_extension, unload_ipython_extension
 from .application.main import main, main_gui
