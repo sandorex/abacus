d6734062cbdcf3a56f0b5744c32405190b174d33
Sandorex <rzhw3h@gmail.com>
2022-04-20 21:35:20 +0200

    More testing stuff

--- abacus/test/test_tokenization.py
+++ abacus/test/test_tokenization.py
@@ -33,3 +33,40 @@ def test_tokenize_untokenize():
 
     assert INPUT == untokenized, 'Untokenization does not match original input'
 
+def test_shift_tokens():
+    # TODO: is this proper way of testing this?
+
+    INCR = 2
+    INDEX = 2
+
+    TOKENS_INPUT = [
+        TokenInfo(tokenize.NAME, 'def', (1, 0), (1, 3), 'def x():\n'),
+        TokenInfo(tokenize.NAME, 'x', (1, 4), (1, 5), 'def x():\n'),
+        TokenInfo(tokenize.OP, '(', (1, 5), (1, 6), 'def x():\n'),
+        TokenInfo(tokenize.OP, ')', (1, 6), (1, 7), 'def x():\n'),
+        TokenInfo(tokenize.OP, ':', (1, 7), (1, 8), 'def x():\n'),
+        TokenInfo(tokenize.NEWLINE, '\n', (1, 8), (1, 9), 'def x():\n'),
+
+        TokenInfo(tokenize.INDENT, '    ', (2, 0), (2, 4), '    pass'),
+        TokenInfo(tokenize.NAME, 'pass', (2, 4), (2, 8), '    pass'),
+        TokenInfo(tokenize.NEWLINE, '', (2, 8), (2, 9), ''),
+        TokenInfo(tokenize.DEDENT, '', (3, 0), (3, 0), ''),
+        TokenInfo(tokenize.ENDMARKER, '', (3, 0), (3, 0), ''),
+    ]
+
+    TOKENS_OUTPUT = TOKENS_INPUT[:INDEX] + [
+        TokenInfo(tokenize.OP, '(', (1, 5 + INCR), (1, 6 + INCR), 'def x():\n'),
+        TokenInfo(tokenize.OP, ')', (1, 6 + INCR), (1, 7 + INCR), 'def x():\n'),
+        TokenInfo(tokenize.OP, ':', (1, 7 + INCR), (1, 8 + INCR), 'def x():\n'),
+        TokenInfo(tokenize.NEWLINE, '\n', (1, 8 + INCR), (1, 9 + INCR), 'def x():\n'),
+
+        TokenInfo(tokenize.INDENT, '    ', (2, 0), (2, 4), '    pass'),
+        TokenInfo(tokenize.NAME, 'pass', (2, 4), (2, 8), '    pass'),
+        TokenInfo(tokenize.NEWLINE, '', (2, 8), (2, 9), ''),
+        TokenInfo(tokenize.DEDENT, '', (3, 0), (3, 0), ''),
+        TokenInfo(tokenize.ENDMARKER, '', (3, 0), (3, 0), ''),
+    ]
+
+    _shift_tokens(TOKENS_INPUT, INDEX, INCR)
+
+    assert TOKENS_INPUT == TOKENS_OUTPUT, 'Tokens arent shifted properly'
