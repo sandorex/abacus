a633ea837e9b70d9a5cd4c1b39d37e7479712bc5
Sandorex (Au) <rzhw3h@gmail.com>
2022-10-10 19:18:42 +0200

    Fix grammar mistake in code

--- abacus/util/lazy_sympy.py
+++ abacus/util/lazy_sympy.py
@@ -5,18 +5,18 @@ from typing import Any
 
 sympy: Any = None
 
-def __deffered_load():
+def __deferred_load():
     global sympy
 
     if sympy is None:
         sympy = import_module('sympy')
 
 def __getattr__(name) -> Any:
-    __deffered_load()
+    __deferred_load()
 
     return getattr(sympy, name)
 
 def __dir__() -> Any:
-    __deffered_load()
+    __deferred_load()
 
     return sympy.__dir__()
